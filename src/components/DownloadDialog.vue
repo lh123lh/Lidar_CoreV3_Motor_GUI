65db64f4d27441f3ffdead386e21f750a04961c4
lh123lh <184591403@qq.com>
2024-06-04 18:29:49 +0800

    优化UpdataDialog和DownloadDialog代码

--- src/components/DownloadDialog.vue
+++ src/components/DownloadDialog.vue
@@ -31,6 +31,10 @@ const props = defineProps({
     type: String,
     required: true
   },
+  status: {
+    type: Boolean,
+    required: false,
+  }
 });
 
 </script>
@@ -40,11 +44,11 @@ const props = defineProps({
     <span>
       <el-row :gutter="10">
         <el-col :span="19">
-          <el-input v-model="filePath" placeholder="选择文件" size="normal" @click="selectUploadFile"
-            :readonly=true></el-input>
+          <el-input v-model="filePath" placeholder="选择文件" size="normal" @click="selectUploadFile" :readonly=true
+            :disabled="status"></el-input>
         </el-col>
         <el-col :span="4">
-          <el-button @click="selectUploadFile" type="success" plain>选择文件</el-button>
+          <el-button @click="selectUploadFile" type="success" plain :disabled="status">选择文件</el-button>
         </el-col>
       </el-row>
 
@@ -52,8 +56,11 @@ const props = defineProps({
     <template #footer>
       <div class="dialog-footer">
         <el-button @click="visable = false">Cancel</el-button>
-        <el-button type="primary" @click="handleDownload(filePath)">
+        <el-button v-if="!status" type="primary" @click="handleDownload(filePath)">
           {{ downloadBtnName }}
+        </el-button>
+        <el-button v-else type="primary" loading>
+          {{ downloadBtnName }}中
         </el-button>
       </div>
     </template>
--- src/components/UploadDialog.vue
+++ src/components/UploadDialog.vue
@@ -39,18 +39,18 @@ const props = defineProps({
     <span>
       <el-row :gutter="10">
         <el-col :span="19">
-          <el-input v-model="filePath" placeholder="选择文件" size="normal" @click="selectUploadFile"
-            :readonly=true></el-input>
+          <el-input v-model="filePath" placeholder="选择文件" size="normal" @click="selectUploadFile" :readonly=true
+            :disabled="status"></el-input>
         </el-col>
         <el-col :span="4">
-          <el-button @click="selectUploadFile" type="success" plain>选择文件</el-button>
+          <el-button @click="selectUploadFile" type="success" plain :disabled="status">选择文件</el-button>
         </el-col>
       </el-row>
 
     </span>
     <template #footer>
       <div class="dialog-footer">
-        <el-button @click="visable = false">Cancel</el-button>
+        <el-button @click="visable = false" :disabled="status">Cancel</el-button>
         <el-button v-if="!status" type="primary" @click="handleUpload(filePath)">
           {{ uploadBtnName }}
         </el-button>
