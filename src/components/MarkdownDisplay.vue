b631e3334443ee0108500d84420ffc715fc7cd0d
lh123lh <184591403@qq.com>
2024-06-21 14:10:40 +0800

    添加marked组件; 添加markdown内容显示组件

---
+++ src/components/MarkdownDisplay.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-html="compiledMarkdown"></div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { marked } from 'marked';
+// import DOMPurify from 'dompurify';
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  markdown: {
+    type: String,
+    required: true
+  }
+});
+
+const compiledMarkdown = computed(() => {
+  // return DOMPurify.sanitize(marked(props.markdown));
+  return marked(props.markdown);
+});
+</script>
